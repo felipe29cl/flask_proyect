03274c58299644aa39b2452ee1bb119f47b96df1
felipe29cl <fcarrascoa29@gmail.com>
2023-12-10 12:01:56 -0300

    first

--- src/app.py
+++ src/app.py
@@ -1,4 +1,29 @@
-from utils import db_connect
-engine = db_connect()
+from flask import Flask, request, jsonify
+import pickle
 
-# your code here
+app = Flask(__name__)
+
+# Cargar el modelo entrenado
+with open("../models/water_model.pkl", "rb") as file:
+    model = pickle.load(file)
+    
+# Ruta para realizar predicciones
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Obtener datos de entrada desde la solicitud POST
+        data = request.get_json()
+
+        # Realizar predicciones utilizando el modelo cargado
+        features = [data['ph'], data['Hardness'], data['Solids'], data['Chloramines'], data['Sulfate'], data['Conductivity'], data['Organic_carbon'], data['Trihalomethanes'], data['Turbidity']]
+        prediction = model.predict([features])[0]
+
+        # Devolver la predicción como JSON
+        result = {'potabilidad_agua': prediction}
+        return jsonify(result)
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
